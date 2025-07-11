1955d791bee013b28ead3abc9c3d3afadb6f990b
KhemkaAkshat <144114211+KhemkaAkshat@users.noreply.github.com>
2025-07-11 23:25:03 +0530

    Connected frontend with backend using api

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,25 @@
-import React from "react";
+"use client"
+import axios from "axios";
+import { useState } from "react";
 
 function Home() {
+
+  const [description, setDescription] = useState("")
+  const [response, setResponse] = useState<{ error?: string; emotion?: string; confidence?: number } | null>(null)
+
+  const handleSubmit = async (e: React.FormEvent)=>{
+    e.preventDefault();
+    try{
+      const res = await axios.post("/api/analysis", {description});
+      setResponse(res.data);
+      console.log(res.data)
+    } catch(err){
+      console.log("API cann failed: ", err)
+    }
+  }
+
+
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-900 px-4">
       <form className="w-full max-w-md bg-white p-8 rounded-2xl shadow-lg flex flex-col gap-6">
@@ -12,16 +31,31 @@ function Home() {
             rows={5}
             placeholder="Type your emotions here"
             className="w-full resize-none rounded-lg border border-gray-300 p-4 text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-400"
+            value = {description}
+            onChange={(e)=>setDescription(e.target.value)}
           ></textarea>
         </div>
 
         <button
           type="submit"
           className="w-full bg-blue-500 text-white font-semibold py-2 rounded-lg hover:bg-blue-600 transition cursor-pointer"
+          onClick={handleSubmit}
         >
           Submit
         </button>
       </form>
+      {response && (
+        <div>
+          {response.error ? (
+            <p>Error: {response.error}</p>
+          ) : (
+            <>
+            <p>Emotion : {response.emotion}</p>
+            <p>Confidence: {response.confidence} </p>
+            </>
+          )}
+        </div>
+      )}
     </div>
   );
 }
