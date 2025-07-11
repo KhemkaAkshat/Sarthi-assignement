f3aa4b1463a83feeec50620518cfcbb3315ff8af
KhemkaAkshat <144114211+KhemkaAkshat@users.noreply.github.com>
2025-07-11 23:31:40 +0530

    Update: backend code added try and catch

--- app/api/analysis/route.ts
+++ app/api/analysis/route.ts
@@ -2,19 +2,21 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
+
   try {
     if (body) {
       return NextResponse.json({ emotion: "Anxious", confidence: 0.85 });
     } else {
+      console.log("Write more about your self ");
       return NextResponse.json(
-        { error: "Write more about yourself" },
+        { error: "Write more about emotions" },
         { status: 400 }
       );
     }
   } catch (err) {
     return NextResponse.json(
-        {error:"Internal server error"},
-        {status:500}
-    )
+      { error: "Internal server error" },
+      { status: 500 }
+    );
   }
 }
